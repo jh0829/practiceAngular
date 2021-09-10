87c0f5fc458d3125be37b8169b7a36d2c1020a7c
jh0829 <j.hino.0829.tec.lucky2@gmail.com>
2021-09-10 23:28:28 +0900

    tsconfig コミット

---
+++ practice-angular/src/app/main/firts-page/firts-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firts-page',
+  templateUrl: './firts-page.component.html',
+  styleUrls: ['./firts-page.component.css']
+})
+export class FirtsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
