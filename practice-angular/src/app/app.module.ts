0d1a18b11f2a56960249094a970a55a266422709
jh0829 <j.hino.0829.tec.lucky2@gmail.com>
2021-09-12 17:08:16 +0900

    ページを色々追加

--- practice-angular/src/app/app.module.ts
+++ practice-angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirtsPageComponent } from './main/firts-page/firts-page.component';
+import { MenuPageComponent } from './main/menu-page/menu-page.component';
+import { GetDataComponent } from './data/get-data/get-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirtsPageComponent
+    FirtsPageComponent,
+    MenuPageComponent,
+    GetDataComponent
   ],
   imports: [
     BrowserModule,
